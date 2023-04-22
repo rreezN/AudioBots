77f1902fc7628daaf99c14208929b477c76edf37
KristianMoellmann <kr_moellmann@hotmail.com>
2023-04-22 14:01:53 +0200

    Small changes

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -99,10 +99,10 @@ def train_sweep() -> None:
 
 
 if __name__ == "__main__":
-    train_sweep()
-    # hparams = {"learning_rate": 1e-3,
-    #            "optimizer": "adam",
-    #            "loss_function": "cross_entropy",
-    #            "activation_function": "ReLU",
-    #            "dropout": 0.3}
-    # train(hparams)
+    # train_sweep()
+    hparams = {"learning_rate": 1e-3,
+               "optimizer": "adam",
+               "loss_function": "cross_entropy",
+               "activation_function": "ReLU",
+               "dropout": 0.3}
+    train(hparams)
